a96bd14bb66185772860d0ef7622f10f0e3089f9
Biel Serrano <bielsesa@gmail.com>
2024-09-06 19:27:09 +0200

    Add pdf cv

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Studies from "@/components/studies";
 import Experience from "@/components/experience";
 import Gamedev from "@/components/gamedev";
 import Header from "@/components/header";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col gap-y-4 bg-bg p-10">
+      <a href="/cv.pdf" download="serrano-biel-CV.pdf"><Button className="fixed right-5 top-5 py-8">Download my CV <br /> as PDF</Button></a>
       <Header />
       <Experience />
       <Gamedev />
