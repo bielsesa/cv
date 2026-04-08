1ea2cdf79cb4e45f54152004535780385f4a1d40
Biel Serrano <bielsesa@gmail.com>
2026-04-08 19:58:53 +0200

    Update with new experience and with copyright 2026

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## My digital CV
 
 A page made in Next.js to contain my current digital CV.
-Still WIP :)
+I try to have it as updated as possible :)
+
+**Last update: 08 April 2026**
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,9 @@
 export default function Footer() {
     return (
         <div className="flex-0.5" id="footer">
-            <p className="text-pink font-light text-center">Biel Serrano © 2024</p>
-            <p className="text-lavender italic font-light text-center">Made with the help of GitHub and Vercel</p>
+            <p className="text-pink font-light text-center">Biel Serrano © 2026</p>
+            <p className="text-lavender italic font-light text-center">Made with the help of GitHub and Vercel :)</p>
+            <p className="text-lavender italic font-light text-center"><a href="https://github.com/bielsesa/cv">You can check the repository of this CV here.</a></p>
         </div>
     );
 }
